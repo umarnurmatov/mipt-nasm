0970db449a5ee46270e793f6f43eb3f52be15cff
Umar Nurmatov <umarkhonnur12@gmail.com>
2026-03-21 22:11:43 +0300

    #ADDED %c support

--- bprintf.s
+++ bprintf.s
@@ -1,25 +1,77 @@
 section .text
 
 extern _printf
+
 global _start
-global _bprintf
+global bprintf
+
+%define NULL_TERM       0x0                   ; null-terminator
+%define WB              2                     ; bytes in word
+%define DB              4                     ; bytes in double word
+%define QB              8                     ; bytes in quad
 
 _start:     
 
-
-_bprintf:       mov     rax, 0x01
-                
-                mov     rdi, 1
-                mov     rsi, Msg
-                mov     rdx, MsgLen
-                syscall
-
+main:           push    word 'x'
+                push    word 'y'
+                push    __FmtStr
+                call    bprintf
 
                 mov     rax, 0x3C
                 xor     rdi, rdi
                 syscall
 
-section     .data
-            
-Msg:        db "__Hllwrld", 0x0a
-MsgLen      equ $ - Msg
+bprintf:        mov     rbp, rsp
+                push    rbp
+
+                mov     rsi,  [rbp+QB]
+                mov     rdi,  __buffer
+                mov     r8, 2*QB            ; first arg offset
+                xor     rdx, rdx            ; length
+
+.fmt_loop:      lodsb
+                
+                cmp     ax, '%'
+                jne     .regular_ch
+
+                lodsb
+                
+                sub     rax, 'a'
+                jmp     [__JmpTbl+rax*DB]
+
+.jmp_c:         mov     al, [rbp+r8]
+                add     r8, WB
+                stosb
+                jmp     .NULL_TERM
+
+.jmp_b:         jmp     .NULL_TERM
+                
+.regular_ch:    stosb
+
+.NULL_TERM:           inc     rdx
+                cmp     ax, NULL_TERM
+                je      .end  
+
+
+                jmp     .fmt_loop
+
+.end:           mov     rax, 1
+                mov     rdi, 0
+                mov     rsi, __buffer
+                ; rdx already equals buffer len
+                syscall
+
+                pop     rbp
+                ret
+
+section .data
+
+__buffer        db 100 dup(0)
+
+section .rodata
+
+__FmtStr        db "%c %c", NULL_TERM
+
+__JmpTbl        dd  0
+                dd  bprintf.jmp_b
+                dd  bprintf.jmp_c
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ BUILD_DIR=build
 PROJ_NAME=bprintf
 
 mkdir -p $BUILD_DIR
-nasm -f elf64 -l $BUILD_DIR/$PROJ_NAME.lst -o $BUILD_DIR/$PROJ_NAME.o $PROJ_NAME.s
-ld -s -o $BUILD_DIR/$PROJ_NAME.out $BUILD_DIR/$PROJ_NAME.o
-./$BUILD_DIR/$PROJ_NAME.out
+nasm -g -f elf64 -l $BUILD_DIR/$PROJ_NAME.lst -o $BUILD_DIR/$PROJ_NAME.o $PROJ_NAME.s
+ld --warn-common -o $BUILD_DIR/$PROJ_NAME $BUILD_DIR/$PROJ_NAME.o
+./$BUILD_DIR/$PROJ_NAME
 
